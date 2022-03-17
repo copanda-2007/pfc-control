c90627460411b965969b380dbce42592675a97fd
copanda-2007 <namaiki.2007@gmail.com>
2022-03-17 16:49:31 +0900

    計算結果をDBに登録

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -6,4 +6,18 @@ class CalculationsController < ApplicationController
   def new
     @calculation = Calculation.new
   end
+
+  def create
+    @calculation = Calculation.new(calculation_params)
+    if @calculation.save
+      redirect_to user_session_path
+    else
+      render :new
+    end
+  end
+
+  private 
+  def calculation_params
+    params.require(:calculation).permit(:weight, :calorie, :protein, :fat, :carbo).merge(user_id: current_user.id)
+  end
 end
